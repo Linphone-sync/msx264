6532dd845c59631a9149e9146ba5cf9426db906b
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-03-13 17:25:34 +0100

    Allow out-of-tree build and use config.h header.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR=`pwd`
+cd $srcdir
+
 #AM_VERSION="1.10"
 if ! type aclocal-$AM_VERSION 1>/dev/null 2>&1; then
 	# automake-1.10 (recommended) is not available on Fedora 8
@@ -29,7 +35,9 @@ echo "Generating build scripts in msx264..."
 set -x
 $LIBTOOLIZE --copy --force
 $ACLOCAL  $ACLOCAL_ARGS
-#autoheader
+autoheader
 $AUTOMAKE --force-missing --add-missing --copy
 autoconf
+
+cd $THEDIR
 
